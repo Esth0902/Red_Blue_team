827809ff30046c27726381373706f175037563b7
WO-VIER <virgile.wautier@gmail.com>
2026-04-12 19:30:46 +0200

    Correction du blocage CORS pour l'API

--- api/self_modules/middlewares/cors.js
+++ api/self_modules/middlewares/cors.js
@@ -1,10 +1,4 @@
-var whitelist = ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:3001']
-
-module.exports = corsOptions = {
-  origin: function (origin, callback) {
-    // Pour ne pas "leaker" l'IP sur GitHub, on accepte temporairement toutes les origines
-    // ou on pourrait rajouter l'IP du VPS dynamique depuis les variables d'environnement.
-    let corsOption = {origin: true}
-    callback(null, corsOption)
-  }
-}
+module.exports = {
+  origin: true,
+  credentials: true
+};
